432674fc8327fa8ad6048c92b82f7558b2fdd2b6
Juan Esteban Londoño Gomez <72523178+Estebanxz11@users.noreply.github.com>
2021-07-06 22:47:27 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ function handlePlay() {
     $video.play();
     $play.hidden = true;
     $pause.hidden = false;
-    console.log('le diste click al boton de play');
+    // console.log('le diste click al boton de play');
     
 }
 
@@ -20,7 +20,7 @@ function handlePause() {
     $video.pause();
     $pause.hidden = true;
     $play.hidden = false;
-    console.log('le diste click al boton de pausa');
+    // console.log('le diste click al boton de pausa');
         
 }
 
@@ -28,14 +28,14 @@ function handlePause() {
 $backward.addEventListener('click', handleBackward );
 function handleBackward() {
     $video.currentTime -= 10 ;
-    console.log('le diste click al boton de retroceder',$video.currentTime);
+    // console.log('le diste click al boton de retroceder',$video.currentTime);
 }
 
 //funcion para adelantar 10 segundos el video
 $forward.addEventListener('click', handleForward );
 function handleForward() {
     $video.currentTime += 10;
-    console.log('le diste click al boton de adelantar',$video.currentTime);
+    // console.log('le diste click al boton de adelantar',$video.currentTime);
 }
 
 const $progress = document.querySelector('#progress'); 
@@ -44,7 +44,7 @@ $video.addEventListener('timeupdate', handleTimeUpdate);
 
 function handleLoaded() {
     $progress.max= $video.duration;
-    console.log('ha cargado mi video', $video.duration);
+    // console.log('ha cargado mi video', $video.duration);
 }
 
 function handleTimeUpdate() {
@@ -56,5 +56,5 @@ $progress.addEventListener('input', handleInput);
 
 function handleInput() {
     $video.currentTime=$progress.value
-    console.log($progress.value);
+    // console.log($progress.value);
 }
